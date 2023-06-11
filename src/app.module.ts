f49bb3d137f7f99a26037d42ef8fc6dcb8119863
samir <arrahmani.samir@gmail.com>
2023-06-11 19:21:41 +0200

    wip add @nest-middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,33 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { FirstMiddleware } from './middlewares/first.middleware';
+import { loggerMiddleware } from './middlewares/logger.middleware';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
 
 @Module({
   imports: [TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(HelmetMiddleware)
+      .forRoutes('*')
+      .apply(FirstMiddleware)
+      .forRoutes(
+        'hello',
+        { path: 'todo', method: RequestMethod.GET },
+        { path: 'todo*', method: RequestMethod.DELETE },
+      )
+      .apply(loggerMiddleware)
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
+
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: ['http://localhost:3001'],
+  };
+  app.enableCors(corsOptions);
+  app.use(morgan('dev'));
+
+  app.use((req: Request, res: Response, next: () => void) => {
+    console.log('Middleware main');
+    next();
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
